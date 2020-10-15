5ae0296fb40e5e34163afbc5c793f72a41311307
Matt Martindale <martindalematt18@yahoo.com>
2020-10-15 14:56:16 -0600

    Update Cell.swift

--- Game of Life/Game of Life/Models/Cell.swift
+++ Game of Life/Game of Life/Models/Cell.swift
@@ -1,1 +1,25 @@
+//
+//  Cell.swift
+//  Game of Life
+//
+//  Created by Matthew Martindale on 10/13/20.
+//
+import Foundation
 
+class Cell {
+  
+  let x: Int, let y: Int
+  var state: State
+  
+  enum State {
+    case alive
+    case dead
+  }
+  
+  init(x: Int, y: Int) {
+    self.x = x
+    self.y = y
+    self.state = .dead
+  }
+  
+}
